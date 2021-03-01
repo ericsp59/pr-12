6b268459077416c4044db0e944257f904580d489
ericsp <ericsp@mail.ru>
2021-03-01 21:13:18 +0300

    add vk token in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,14 @@
+// ID приложения:	7776960
+// защищенный ключ: 7lMhvnkRbgePQcRlF5hR
+// сервисный ключ доступа: ae7cf907ae7cf907ae7cf90769ae0a53c7aae7cae7cf907ce41f69c1490abcc0d1a7ff2
+// access_token=06cd732761927982256873cd35f908ffb94d9413d57810b55b1a767472ed753d52ebcf083e0b2a6c79226
+// expires_in=86400
+// user_id=16462017
+
+
+// запрос на получение токена:
+// https://oauth.vk.com/authorize?client_id=7776960&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52
+
+
 import * as axios from 'axios';
 
